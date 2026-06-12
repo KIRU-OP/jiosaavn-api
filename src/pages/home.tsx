55c7af8a2b28772afcd4050c547366537e0655bb
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-06-12 12:29:14 +0530

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -43,16 +43,16 @@ Home.get('/', (c) => {
         <meta property="twitter:description" content={description} />
         <meta
           property="og:image"
-          content="https://raw.githubusercontent.com/sumitkolhe/jiosaavn-api/main/assets/preview.jpg"
+          content="https://raw.githubusercontent.com/KIRU-OP/jiosaavn-api/main/assets/preview.jpg"
         />
         <meta
           property="twitter:image"
-          content="https://raw.githubusercontent.com/sumitkolhe/jiosaavn-api/main/assets/preview.jpg"
+          content="https://raw.githubusercontent.com/KIRU-OP/jiosaavn-api/main/assets/preview.jpg"
         />
         <link
           rel="icon"
           type="image/x-icon"
-          href="https://raw.githubusercontent.com/sumitkolhe/jiosaavn-api/main/assets/favicon.ico"
+          href="https://raw.githubusercontent.com/KIRU-OP/jiosaavn-api/main/assets/favicon.ico"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
@@ -311,7 +311,7 @@ Home.get('/', (c) => {
             <a
               target="_blank"
               class="p-4 sm:p-8 hover:bg-opacity-5 hover:bg-white rounded-lg duration-100 sm:col-span-4"
-              href="https://github.com/sumitkolhe/jiosaavn-api"
+              href="https://github.com/KIRU-OP/jiosaavn-api"
             >
               <div class="flex flex-col">
                 <span class="text-xs uppercase bg-opacity-15 rounded text-center max-w-fit px-2 py-1 font-bold tracking-wide bg-green-500 text-green-500">
@@ -325,7 +325,7 @@ Home.get('/', (c) => {
             <a
               target="_blank"
               class="p-4 sm:p-8 hover:bg-opacity-5 hover:bg-white rounded-lg duration-100 sm:col-span-4"
-              href="https://github.com/sumitkolhe/jiosaavn-api/issues"
+              href="https://github.com/KIRU-OP/jiosaavn-api/issues"
             >
               <div class="flex flex-col">
                 <span class="text-xs uppercase bg-opacity-15 rounded text-center max-w-fit px-2 py-1 font-bold tracking-wide bg-violet-500 text-violet-500">
@@ -347,11 +347,11 @@ Home.get('/', (c) => {
                 <span class="text-neutral-200 font-bold text-lg sm:text-xl md:text-2xl mt-2">Sumit Kolhe</span>
                 <div class="text-neutral-500 mt-2">
                   Have a question or need help? Reach out on{' '}
-                  <a href="https://github.com/sumitkolhe" target="_blank" rel="noopener noreferrer" class="hover:underline text-indigo-500">GitHub</a>
+                  <a href="https://github.com/kiru-op" target="_blank" rel="noopener noreferrer" class="hover:underline text-indigo-500">GitHub</a>
                   ,{' '}
-                  <a href="https://twitter.com/thesumitkolhe" target="_blank" rel="noopener noreferrer" class="hover:underline text-sky-500">Twitter</a>
+                  <a href="https://github.com/kiru-op" target="_blank" rel="noopener noreferrer" class="hover:underline text-sky-500">Twitter</a>
                   , or{' '}
-                  <a href="https://t.me/sumitkolhe" target="_blank" rel="noopener noreferrer" class="hover:underline text-pink-500">Telegram.</a>
+                  <a href="https://t.me/kiru_op" target="_blank" rel="noopener noreferrer" class="hover:underline text-pink-500">Telegram.</a>
                 </div>
               </div>
             </div>
